8c930d8e6af1caf914943762a0912195502494cf
kokoreva.a2 <kokoreva.a2@rwb.ru>
2025-05-14 13:56:10 +0300

    hw5

--- homework/functions/homework_test.go
+++ homework/functions/homework_test.go
@@ -8,18 +8,38 @@ import (
 )
 
 func Map(data []int, action func(int) int) []int {
-	// need to implement
-	return nil
+	if data == nil {
+		return nil
+	}
+	result := make([]int, len(data))
+	for i, v := range data {
+		result[i] = action(v)
+	}
+	return result
 }
 
 func Filter(data []int, action func(int) bool) []int {
-	// need to implement
-	return nil
+	if data == nil {
+		return nil
+	}
+	result := make([]int, 0, len(data))
+	for _, v := range data {
+		if action(v) {
+			result = append(result, v)
+		}
+	}
+	if len(result) == 0 && len(data) == 0 {
+		return []int{}
+	}
+	return result
 }
 
 func Reduce(data []int, initial int, action func(int, int) int) int {
-	// need to implement
-	return 0
+	result := initial
+	for _, v := range data {
+		result = action(result, v)
+	}
+	return result
 }
 
 func TestMap(t *testing.T) {

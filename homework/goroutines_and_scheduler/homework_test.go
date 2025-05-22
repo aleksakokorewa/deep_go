adf41dfa6b74e663af1b7c27bb406805fd8dc3f1
kokoreva.a2 <kokoreva.a2@rwb.ru>
2025-05-22 22:53:15 +0300

    hw12

--- homework/goroutines_and_scheduler/homework_test.go
+++ homework/goroutines_and_scheduler/homework_test.go
@@ -1,36 +1,88 @@
 package main
 
 import (
-	"testing"
-
+	"container/heap"
 	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
 type Task struct {
 	Identifier int
 	Priority   int
+	index      int // служебное поле для кучи
+}
+
+type taskHeap []*Task
+
+func (h taskHeap) Len() int { return len(h) }
+
+func (h taskHeap) Less(i, j int) bool {
+	return h[i].Priority > h[j].Priority // max-heap
+}
+
+func (h taskHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+	h[i].index = i
+	h[j].index = j
+}
+
+func (h *taskHeap) Push(x any) {
+	task := x.(*Task)
+	task.index = len(*h)
+	*h = append(*h, task)
+}
+
+func (h *taskHeap) Pop() any {
+	old := *h
+	n := len(old)
+	task := old[n-1]
+	*h = old[:n-1]
+	return task
 }
 
 type Scheduler struct {
-	// need to implement
+	heap      taskHeap
+	active    map[int]*Task
+	originals map[int]Task
 }
 
 func NewScheduler() Scheduler {
-	// need to implement
-	return Scheduler{}
+	return Scheduler{
+		heap:      make(taskHeap, 0),
+		active:    make(map[int]*Task),
+		originals: make(map[int]Task),
+	}
 }
 
 func (s *Scheduler) AddTask(task Task) {
-	// need to implement
+	if _, exists := s.active[task.Identifier]; exists {
+		return
+	}
+	t := &Task{
+		Identifier: task.Identifier,
+		Priority:   task.Priority,
+	}
+	s.originals[task.Identifier] = task
+	s.active[task.Identifier] = t
+	heap.Push(&s.heap, t)
 }
 
 func (s *Scheduler) ChangeTaskPriority(taskID int, newPriority int) {
-	// need to implement
+	if t, ok := s.active[taskID]; ok {
+		t.Priority = newPriority
+		heap.Fix(&s.heap, t.index)
+	}
 }
 
 func (s *Scheduler) GetTask() Task {
-	// need to implement
-	return Task{}
+	if len(s.heap) == 0 {
+		return Task{}
+	}
+	t := heap.Pop(&s.heap).(*Task)
+	delete(s.active, t.Identifier)
+	original := s.originals[t.Identifier]
+	delete(s.originals, t.Identifier)
+	return original
 }
 
 func TestTrace(t *testing.T) {
